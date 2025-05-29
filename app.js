7ebab73cfc55f4cd36e387078e7e11a6c2eaabbe
rarelycalmya <chankamya2706@gmail.com>
2025-05-29 09:14:44 +0530

    Name, selection and summary through html only

--- app.js
+++ app.js
@@ -1,9 +1,58 @@
-var app = angular.module('CollectGameApp', []);
+var app = angular.module('CollectGameApp', ['ngRoute']);
 
-app.controller('GameController', function($scope) {
-  $scope.inventory = [];
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'enter.html',
+      controller: 'MainController'
+    })
+    .when('/game', {
+      templateUrl: 'game.html',
+      controller: 'GameController'
+    })
+    .when('/summary', {
+      templateUrl: 'summary.html',
+      controller: 'SummaryController'
+    })
+    .otherwise({ redirectTo: '/' });
+});
 
-  // Define collectible classes
+app.factory('UserService', function() {
+  var users = [];
+  var currentUser = null;
+
+  return {
+    setCurrentUser: function(name) {
+      currentUser = { name: name, inventory: [] };
+      users.push(currentUser);
+    },
+    getCurrentUser: function() {
+      return currentUser;
+    },
+    addItemToCurrentUser: function(item) {
+      if (currentUser) {
+        currentUser.inventory.push(item);
+      }
+    },
+    getAllUsers: function() {
+      return users;
+    }
+  };
+});
+
+app.controller('MainController', function($scope, $location, UserService) {
+  $scope.startGame = function() {
+    if ($scope.userName && $scope.userName.trim() !== '') {
+      UserService.setCurrentUser($scope.userName.trim());
+      $location.path('/game');
+    }
+  };
+});
+
+app.controller('GameController', function($scope, $location, UserService) {
+  $scope.currentUser = UserService.getCurrentUser();
+  $scope.inventory = $scope.currentUser.inventory;
+
   function Apple() {
     this.name = "Apple";
     console.log("Created new Apple object");
@@ -14,29 +63,33 @@ app.controller('GameController', function($scope) {
     console.log("Created new Banana object");
   }
 
-  // Map class names to constructors
   var classMap = {
     'Apple': Apple,
     'Banana': Banana
   };
 
-  // Called when dragging starts
   $scope.dragStart = function(event, className) {
     event.dataTransfer.setData("text/plain", className);
     console.log("Dragging:", className);
   };
 
-  // Called when dropped into inventory
   $scope.onDrop = function(event) {
     event.preventDefault();
     var className = event.dataTransfer.getData("text/plain");
     var constructorFn = classMap[className];
-
     if (constructorFn) {
       var obj = new constructorFn();
       $scope.$apply(function() {
-        $scope.inventory.push(obj);
+        UserService.addItemToCurrentUser(obj);
       });
     }
   };
+
+  $scope.submitInventory = function() {
+    $location.path('/summary');
+  };
+});
+
+app.controller('SummaryController', function($scope, UserService) {
+  $scope.allUsers = UserService.getAllUsers();
 });
