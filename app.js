d10e7c817a3e374afe99b3540953e6083ff47491
rarelycalmya <chankamya2706@gmail.com>
2025-05-29 09:09:47 +0530

    Converted fts into classes

--- app.js
+++ app.js
@@ -3,17 +3,40 @@ var app = angular.module('CollectGameApp', []);
 app.controller('GameController', function($scope) {
   $scope.inventory = [];
 
-  $scope.dragStart = function(event, itemName) {
-    event.dataTransfer.setData("text/plain", itemName);
+  // Define collectible classes
+  function Apple() {
+    this.name = "Apple";
+    console.log("Created new Apple object");
+  }
+
+  function Banana() {
+    this.name = "Banana";
+    console.log("Created new Banana object");
+  }
+
+  // Map class names to constructors
+  var classMap = {
+    'Apple': Apple,
+    'Banana': Banana
   };
 
+  // Called when dragging starts
+  $scope.dragStart = function(event, className) {
+    event.dataTransfer.setData("text/plain", className);
+    console.log("Dragging:", className);
+  };
+
+  // Called when dropped into inventory
   $scope.onDrop = function(event) {
     event.preventDefault();
-    var item = event.dataTransfer.getData("text/plain");
+    var className = event.dataTransfer.getData("text/plain");
+    var constructorFn = classMap[className];
 
-    // Angular doesn't automatically know to update view from native events
-    $scope.$apply(function() {
-      $scope.inventory.push(item);
-    });
+    if (constructorFn) {
+      var obj = new constructorFn();
+      $scope.$apply(function() {
+        $scope.inventory.push(obj);
+      });
+    }
   };
 });
