5aa21f1102ed5859185728f425b342934d623f85
rarelycalmya <chankamya2706@gmail.com>
2025-05-29 09:17:37 +0530

    Added more users: lesser html files, more angular js work

--- app.js
+++ app.js
@@ -1,95 +1,55 @@
-var app = angular.module('CollectGameApp', ['ngRoute']);
+angular.module('CollectGameApp', [])
+  .controller('MainController', ['$scope', function($scope) {
+    $scope.page = 'enter'; // enter | game | summary
+    $scope.userName = '';
+    $scope.allUsers = [];
+    $scope.currentUser = null;
 
-app.config(function($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'enter.html',
-      controller: 'MainController'
-    })
-    .when('/game', {
-      templateUrl: 'game.html',
-      controller: 'GameController'
-    })
-    .when('/summary', {
-      templateUrl: 'summary.html',
-      controller: 'SummaryController'
-    })
-    .otherwise({ redirectTo: '/' });
-});
+    // Classes for collectibles
+    function Apple() {
+      this.name = "Apple";
+      console.log("Created new Apple object");
+    }
+    function Banana() {
+      this.name = "Banana";
+      console.log("Created new Banana object");
+    }
 
-app.factory('UserService', function() {
-  var users = [];
-  var currentUser = null;
+    var classMap = {
+      'Apple': Apple,
+      'Banana': Banana
+    };
 
-  return {
-    setCurrentUser: function(name) {
-      currentUser = { name: name, inventory: [] };
-      users.push(currentUser);
-    },
-    getCurrentUser: function() {
-      return currentUser;
-    },
-    addItemToCurrentUser: function(item) {
-      if (currentUser) {
-        currentUser.inventory.push(item);
+    $scope.startGame = function() {
+      $scope.currentUser = { name: $scope.userName.trim(), inventory: [] };
+      $scope.allUsers.push($scope.currentUser);
+      $scope.page = 'game';
+    };
+
+    $scope.dragStart = function(event, className) {
+      event.dataTransfer.setData("text/plain", className);
+      console.log("Dragging:", className);
+    };
+
+    $scope.onDrop = function(event) {
+      event.preventDefault();
+      var className = event.dataTransfer.getData("text/plain");
+      var ConstructorFn = classMap[className];
+      if (ConstructorFn) {
+        var obj = new ConstructorFn();
+        $scope.$apply(function() {
+          $scope.currentUser.inventory.push(obj);
+        });
       }
-    },
-    getAllUsers: function() {
-      return users;
-    }
-  };
-});
+    };
 
-app.controller('MainController', function($scope, $location, UserService) {
-  $scope.startGame = function() {
-    if ($scope.userName && $scope.userName.trim() !== '') {
-      UserService.setCurrentUser($scope.userName.trim());
-      $location.path('/game');
-    }
-  };
-});
+    $scope.goToSummary = function() {
+      $scope.page = 'summary';
+    };
 
-app.controller('GameController', function($scope, $location, UserService) {
-  $scope.currentUser = UserService.getCurrentUser();
-  $scope.inventory = $scope.currentUser.inventory;
-
-  function Apple() {
-    this.name = "Apple";
-    console.log("Created new Apple object");
-  }
-
-  function Banana() {
-    this.name = "Banana";
-    console.log("Created new Banana object");
-  }
-
-  var classMap = {
-    'Apple': Apple,
-    'Banana': Banana
-  };
-
-  $scope.dragStart = function(event, className) {
-    event.dataTransfer.setData("text/plain", className);
-    console.log("Dragging:", className);
-  };
-
-  $scope.onDrop = function(event) {
-    event.preventDefault();
-    var className = event.dataTransfer.getData("text/plain");
-    var constructorFn = classMap[className];
-    if (constructorFn) {
-      var obj = new constructorFn();
-      $scope.$apply(function() {
-        UserService.addItemToCurrentUser(obj);
-      });
-    }
-  };
-
-  $scope.submitInventory = function() {
-    $location.path('/summary');
-  };
-});
-
-app.controller('SummaryController', function($scope, UserService) {
-  $scope.allUsers = UserService.getAllUsers();
-});
+    $scope.resetGame = function() {
+      $scope.userName = '';
+      $scope.currentUser = null;
+      $scope.page = 'enter';
+    };
+  }]);
